3ebe23dfb0a23d6066eeeb11f707c3f64ac0f61a
Aleksander Bekrenev <praise2001@yandex.ru>
2022-04-08 12:07:18 +0700

    Вывод доминантных цветов на экран

--- ColorFinder/ViewModels/MainWindowViewModel.cs
+++ ColorFinder/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+using ColorFinder.Models;
 using ColorFinder.Services;
-using Microsoft.Win32;
 using Prism.Commands;
 using Prism.Mvvm;
+using Brush = System.Windows.Media.Brush;
+using Color = System.Windows.Media.Color;
 
 namespace ColorFinder.ViewModels
 {
@@ -13,22 +15,64 @@ namespace ColorFinder.ViewModels
         public MainWindowViewModel()
         {
             _imageUpload = new ImageUploadService();
+            _colorCounter = new ColorCounter();
             ImageUploadCommand = new DelegateCommand(ImageUploadCommandExecute);
+            
+            var standardColor = Color.FromArgb(100, 196, 196, 196);
+            
+            Brush1 = new SolidColorBrush(standardColor);
+            Brush2 = new SolidColorBrush(standardColor);
+            Brush3 = new SolidColorBrush(standardColor);
         }
 
-        private readonly ImageUploadService _imageUpload;
+        public DelegateCommand ImageUploadCommand { get; }
         
+        private readonly ImageUploadService _imageUpload;
+        private readonly ColorCounter _colorCounter;
+        private string _mainImage;
+
+        private Brush _brush1;
+        private Brush _brush2;
+        private Brush _brush3;
+        
+        public Brush Brush1
+        {
+            get => _brush1;
+            set => SetProperty(ref _brush1, value);
+        }
+        public Brush Brush2
+        {
+            get => _brush2;
+            set => SetProperty(ref _brush2, value);
+        }
+        
+        public Brush Brush3
+        {
+            get => _brush3;
+            set => SetProperty(ref _brush3, value);
+        }
+
         public string MainImage
         {
             get => _mainImage;
             set => SetProperty(ref _mainImage, value);
         }
-
-        private string _mainImage;
-        public DelegateCommand ImageUploadCommand { get; }
+        
         private void ImageUploadCommandExecute()
         {
             MainImage = _imageUpload.GetImageFileName();
+
+            var mediaColors = new List<Color>();
+            var dominantColors = _colorCounter.GetDominantColors(MainImage);
+
+            foreach (var color in dominantColors)
+            {
+                mediaColors.Add(System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B));
+            }
+            
+            Brush1 = new SolidColorBrush(mediaColors[0]);
+            Brush2 = new SolidColorBrush(mediaColors[1]);
+            Brush3 = new SolidColorBrush(mediaColors[2]);
         }
     }
 }
