97214505fcbf782a9e763f72063d21f64004d88b
Aleksander Bekrenev <praise2001@yandex.ru>
2022-04-15 23:29:03 +0700

    add: rgb and hex interpretation

--- ColorFinder/ViewModels/MainWindowViewModel.cs
+++ ColorFinder/ViewModels/MainWindowViewModel.cs
@@ -28,6 +28,8 @@ namespace ColorFinder.ViewModels
             Color3 = new SolidColorBrush(standardColor);
             
             _interpretation = ColorInterpretations.First();
+
+            OnSetColorInterpretationEvent += SetColorsInTextBlocks;
         }
         #endregion
 
@@ -41,30 +43,34 @@ namespace ColorFinder.ViewModels
                 return;
             }
             
-            var mediaColors = new List<Color>();
-            var dominantColors = await _colorCounter.GetDominantColors(imageFilePath);
+            _mediaColors = new List<Color>();
+            _dominantColors = await _colorCounter.GetDominantColors(imageFilePath);
 
-            foreach (var color in dominantColors)
+            foreach (var color in _dominantColors)
             {
-                mediaColors.Add(Color.FromArgb(color.A, color.R, color.G, color.B));
+                _mediaColors.Add(Color.FromArgb(color.A, color.R, color.G, color.B));
             }
             
-            Color1 = new SolidColorBrush(mediaColors[0]);
-            Color2 = new SolidColorBrush(mediaColors[1]);
-            Color3 = new SolidColorBrush(mediaColors[2]);
+            Color1 = new SolidColorBrush(_mediaColors[0]);
+            Color2 = new SolidColorBrush(_mediaColors[1]);
+            Color3 = new SolidColorBrush(_mediaColors[2]);
             MainImage = imageFilePath;
 
             var random = new Random();
-            var randomDominantColor = random.Next(mediaColors.Count);
-            var titleColor = mediaColors[randomDominantColor];
+            var randomDominantColor = random.Next(_mediaColors.Count);
+            var titleColor = _mediaColors[randomDominantColor];
 
             TitleBarBrush = new SolidColorBrush(titleColor);
-            // TitleBarTextBrush = new SolidColorBrush();
+            
+            OnSetColorInterpretationEvent.Invoke();
         }
 
         #endregion
 
         #region PrivateFields
+
+        private List<System.Drawing.Color> _dominantColors;
+        private List<Color> _mediaColors;
         
         private int _outerMarginSize = 10;
         private int _windowRadius = 10;
@@ -76,6 +82,10 @@ namespace ColorFinder.ViewModels
         private Brush? _color1;
         private Brush? _color2;
         private Brush? _color3;
+
+        private string _colorInterpretation1 = "";
+        private string _colorInterpretation2 = "";
+        private string _colorInterpretation3 = "";
 
         private Brush _titleBarBrush;
         private Brush _titleBarTextBrush = new SolidColorBrush(Color.FromRgb(104, 104, 104));
@@ -91,6 +101,7 @@ namespace ColorFinder.ViewModels
             get => _color1;
             set => SetProperty(ref _color1, value);
         }
+        
         public Brush Color2
         {
             get => _color2;
@@ -103,11 +114,30 @@ namespace ColorFinder.ViewModels
             set => SetProperty(ref _color3, value);
         }
 
+        public string ColorInterpretation1
+        {
+            get => _colorInterpretation1;
+            set => SetProperty(ref _colorInterpretation1, value);
+        }
+        
+        public string ColorInterpretation2
+        {
+            get => _colorInterpretation2;
+            set => SetProperty(ref _colorInterpretation2, value);
+        }
+        
+        public string ColorInterpretation3
+        {
+            get => _colorInterpretation3;
+            set => SetProperty(ref _colorInterpretation3, value);
+        }
+
         public string MainImage
         {
             get => _mainImage;
             set => SetProperty(ref _mainImage, value);
         }
+        
         public int OuterMarginSize => _outerMarginSize;
         
         public int WindowRadius => _windowRadius;
@@ -138,11 +168,48 @@ namespace ColorFinder.ViewModels
             set
             {
                 SetProperty(ref _interpretation, value);
-                
+                OnSetColorInterpretationEvent.Invoke();
             }
         }
 
         #endregion
 
+        #region Events
+
+        private delegate void SetColorInterpretation();
+        private event SetColorInterpretation OnSetColorInterpretationEvent;
+
+        #endregion
+
+        #region Private Methods
+
+        private void SetColorsInTextBlocks()
+        {
+            var color1 = _dominantColors[0];
+            var color2 = _dominantColors[1];
+            var color3 = _dominantColors[2];
+            
+            switch (SelectedColorInterpretation)
+            {
+                case "Rgb":
+                    ColorInterpretation1 = $"RGB: ({color1.R}, {color1.G}, {color1.B})";
+                    ColorInterpretation2 = $"RGB: ({color2.R}, {color2.G}, {color2.B})";
+                    ColorInterpretation3 = $"RGB: ({color3.R}, {color3.G}, {color3.B})";
+                    break;
+                case "Hex":
+                    ColorInterpretation1 = "#" + color1.R.ToString("X2") + 
+                                           color1.G.ToString("X2") + 
+                                           color1.B.ToString("X2");
+                    ColorInterpretation2 = "#" + color2.R.ToString("X2") + 
+                                           color2.G.ToString("X2") + 
+                                           color2.B.ToString("X2");
+                    ColorInterpretation3 = "#" + color3.R.ToString("X2") + 
+                                           color3.G.ToString("X2") + 
+                                           color3.B.ToString("X2");
+                    break;
+            }
+        }
+
+        #endregion
     }
 }
