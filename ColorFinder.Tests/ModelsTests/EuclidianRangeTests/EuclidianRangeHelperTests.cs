f66cd1358622bf36809b9901d0f3bab49bdd0179
Aleksander Bekrenev <praise2001@yandex.ru>
2022-04-26 00:03:39 +0700

    feat: added tests to EuclidianRangeHelper

---
+++ ColorFinder.Tests/ModelsTests/EuclidianRangeTests/EuclidianRangeHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using ColorFinder.Models;
+using NUnit.Framework;
+
+namespace ColorFinderTests.ModelsTests.EuclidianRangeTests
+{
+    public class EuclidianRangeHelperTests
+    {
+        [Test]
+        public void EuclidianRange_SameColors_ZeroReturned()
+        {
+            const double expectedRange = 0.0;
+
+            var actualRange = EuclidianRangeHelper.EuclidianRange(Color.Black, Color.Black);
+
+            Assert.AreEqual(expectedRange, actualRange);
+        }
+
+        [Test]
+        public void EuclidianRange_BlueIsColor1_RedIsColor2_realRangeReturned()
+        {
+            const double expectedRange = 360.624458;
+
+            var actualRange = EuclidianRangeHelper.EuclidianRange(Color.Blue, Color.Red);
+
+            Assert.AreEqual(expectedRange, Math.Round(actualRange, 6));
+        }
+    }
+}
