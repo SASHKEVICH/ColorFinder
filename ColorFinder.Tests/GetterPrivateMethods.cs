2efa18daf13a8f56b3735d68777fca8c7844fa7b
Aleksander Bekrenev <praise2001@yandex.ru>
2022-04-26 21:21:36 +0700

    feat: tests to kcluster

---
+++ ColorFinder.Tests/GetterPrivateMethods.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+
+namespace ColorFinderTests
+{
+    public static class GetterPrivateMethods
+    {
+        public static MethodInfo GetMethod<T>(T testObject, string methodName)
+        {
+            if (string.IsNullOrWhiteSpace(methodName))
+            {
+                Assert.Fail("methodName cannot be null or white space");
+            }
+
+            if (testObject == null)
+            {
+                Assert.Fail($"{nameof(testObject)} is null");
+            }
+
+            var method = testObject.GetType().GetMethod(
+                methodName, BindingFlags.NonPublic | BindingFlags.Instance
+            );
+
+            if (method == null)
+            {
+                Assert.Fail($"{methodName} method not found");
+            }
+
+            return method;
+        }
+    }
+}
---
+++ ColorFinder.Tests/ModelsTests/KMeansTests/KClusterTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using ColorFinder.Models.KMeans;
+using NUnit.Framework;
+
+namespace ColorFinderTests.ModelsTests.KMeansTests
+{
+    public class KClusterTests
+    {
+        private KCluster? _testCluster;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _testCluster = new KCluster(Color.Black);
+        }
+        
+        [Test]
+        public void RecalculateCenter_BlackColorIsCenter_clustersColorEmpty_BlackNewCenterReturned()
+        {
+            var method = GetterPrivateMethods.GetMethod<KCluster>(_testCluster,"RecalculateCenter");
+            
+            var expectedValue = Color.FromArgb(255, 0, 0,0);
+
+            var actualValue = method.Invoke(_testCluster, null);
+            
+            Assert.That(actualValue, Is.Not.Null);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+        
+        [Test]
+        public void RecalculateCenter_ClustersColorsFilled_NearlyPinkReturned()
+        {
+            var method = GetterPrivateMethods.GetMethod<KCluster>(_testCluster,"RecalculateCenter");
+
+            var expectedColor = Color.FromArgb(255, 170, 85, 85);
+            
+            var clusterColors = new List<Color>
+            {
+                Color.Black,
+                Color.White,
+                Color.Red
+            };
+            
+            clusterColors.ForEach(color => _testCluster.AddColor(color));
+
+            var actualValue = method.Invoke(_testCluster, null);
+            
+            Assert.That(actualValue, Is.Not.Null);
+            Assert.AreEqual(expectedColor, actualValue);
+        }
+
+        [Test]
+        public void GetRangeFromCurrentCenterToNew_ClusterColorsFilled_BlackIsOldCenter_RangeToNewCenterReturned()
+        {
+            var method = GetterPrivateMethods.GetMethod<KCluster>(_testCluster,"GetRangeFromCurrentCenterToNew");
+
+            var expectedValue = 284.726;
+            
+            var clusterColors = new List<Color>
+            {
+                Color.Chartreuse,
+                Color.White,
+                Color.Red
+            };
+            
+            clusterColors.ForEach(color => _testCluster.AddColor(color));
+
+            var actualValue = (double) method.Invoke(_testCluster, null);
+
+            Assert.That(actualValue, Is.Not.Null);
+            Assert.AreEqual(expectedValue, Math.Round(actualValue, 3));
+        }
+    }
+}
